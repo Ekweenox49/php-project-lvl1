23898c1bb445dd2f4c2e4a01ca1c496232d4cd61
anna.gavrilova <anna.gavrilova@smartmail.biz>
2021-12-28 14:18:33 +0300

    Fix for tests

--- src/Games/Cli.php
+++ src/Games/Cli.php
@@ -10,5 +10,4 @@ function sayHi()
      line('Welcome to the Brain Game!');
      $name = prompt('May I have your name?');
      line("Hello, %s!", $name);
-     line('Welcome to the Brain Game!');
 }
